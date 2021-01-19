f2fafc631dff94a40a5bec9d25dbcb398e61aeca
miloscomplex <hicallmesutton@gmail.com>
2021-01-19 01:21:58 -0600

    commit tree c49fd8c6607007fbc26cecf3f4ed5fc43ad8d559

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, funcBlock) {
+  let resultsArray = []
+  array.forEach(element => {
+    resultsArray.push(funcBlock(element))
+  })
+  return resultsArray
+}
+
+function reduce(array, funcBlock, memo = 0 ) {
+  array.forEach(element => {
+    if (isNaN(element)) {
+      memo = true
+    }
+    memo = funcBlock(element, memo)
+    console.log("memo= " + memo);
+  })
+  return memo
+}
